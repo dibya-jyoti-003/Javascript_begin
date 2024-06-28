ef66fad349023c48b08415aa58fbf0367907d480
Dibyajyoti Karmakar <102862431+dibya-jyoti-003@users.noreply.github.com>
2024-06-28 11:25:27 +0000

    conversions

---
+++ 01_basics/conversion.js
@@ -0,0 +1,58 @@
+let score = "33gh"
+score =null
+score =undefined 
+score ="dibya"
+
+/*console.log(typeof score )
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber )
+console.log(valueInNumber)*/
+
+//easily converts number
+/*
+"33" - 33
+"33fkl" - NaN
+*/ 
+
+
+/*
+let isLogged = "hello"
+let booleanLogged = Boolean(isLogged)
+console.log(typeof booleanLogged)
+console.log(booleanLogged)
+
+let someNumber  = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+*/
+
+
+
+// operations 
+let value  = 3
+let negNum = -value
+//console.log(negNum);
+
+let st1 = "dibyajyoti"
+let st2 = "karmakar"
+let st3 =st1 +" " +st2
+//console.log(st3);
+
+/*
+console.log(1+ "2");
+console.log(2+2+"1");
+console.log(+true);
+console.log(+"");
+*/
+
+let num1,num2,num3
+
+num1=num2=num3 = 2+2
+let gamecounter = 100
+let pre = ++gamecounter
+let post = gamecounter++
+console.log(gamecounter);
+console.log(pre);
+console.log(post);
