9f7bfdb1ace7069a14ff26acc2c207ffd20b5a76
Dibyajyoti Karmakar <102862431+dibya-jyoti-003@users.noreply.github.com>
2024-06-28 11:06:19 +0000

    variables and datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using node js not browser 
+
+//console.log(3+3)
+
+let name ="Dibyajyoti"
+let age = 20
+let isLoggedIn =true
+let state = null
+
+// number =>2 to the power 53
+//big int
+// string =""
+//boolean =>true/false
+//null->datatype as well as stand alone value
+//undefined
+//symbol => uniqueness
+
+// object 
+
+console.log(typeof name )
+console.log(typeof null )
+console.log(typeof undefined )
---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 12512
+
+//accountId = 45
+
+console.log(accountId);
+
+let accountName = "Dibyajyoti Karmakar"
+let accountState ;
+let accCity = "Kolkata", accountAge = 20
+
+console.table([accountName,accountAge,accountState])
