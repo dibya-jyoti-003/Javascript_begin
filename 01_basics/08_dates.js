5e4382162232b78e3772bbb07725fee3077e909c
Dibyajyoti Karmakar <102862431+dibya-jyoti-003@users.noreply.github.com>
2024-06-28 23:01:07 +0000

    13

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,29 @@
+let mydate = new Date()
+/*
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleTimeString());
+console.log(typeof mydate);
+*/
+let newDate = new Date(2023,0,23)
+/*
+console.log(newDate);
+console.log(newDate.toDateString());
+console.log(newDate.toString());*/
+
+let myTime = Date.now()
+/*
+console.log(myTime);
+console.log(newDate.getTime());
+
+console.log(mydate.toString());
+console.log(mydate.getDay());
+console.log(mydate.getHours());
+console.log(mydate.getMonth());
+console.log(mydate.getTime());
+console.log(mydate.getUTCDay());
+console.log(mydate.getFullYear());
+*/
